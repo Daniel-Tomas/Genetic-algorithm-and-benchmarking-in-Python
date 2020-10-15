1b81c5cf45ac4c9b02936638ae0cc6235ee6b086
pabelsbf <pabelspbf@gmail.com>
2020-10-15 21:16:04 +0200

    Population - Comentarios y cambio en replace

--- group14/Population.py
+++ group14/Population.py
@@ -2,12 +2,21 @@ from group14.Genome import Genome
 
 
 class Population:
+    """Basic class
+
+        It creates a collection of Genomes.
+
+        Atributes:
+                Genome (array): Genome class object
+        """
+
     def __init__(self, collection):
         self.collection = collection
 
     def __repr__(self):
         """
-        It casts ...
+        It casts the Population elements in String format.
+
         Returns:
              str: The population in string format
         """
@@ -17,38 +26,46 @@ class Population:
         return res
 
     def ascendent_sort(self):
+        """It sort Genomes of 'self' by fitness in ascending order.
+        """
         self.collection.sort(key=lambda x: x.fitness)
 
+
     def descendent_sort(self):
+        """It sort Genomes of 'self' by fitness in descending order.
+        """
         self.collection.sort(key=lambda x: x.fitness, reverse=True)
 
     def add(self, genome):
-        """
+        """Add an element of type Genome to the end of 'self'
         Args:
-            genome:
+            genome(array): Genome class object
         """
         self.collection.append(genome)
 
     def remove(self, genome):
-        """Excepcion en caso de que no esté el elemento a elminar
+        """Remove the genome passed as an argument to 'self'.
+        Excepcion en caso de que no esté el elemento a elminar
 
         Args:
-            genome:
+            genome(array): Genome class object
         """
         # for elem in self.collection:
         #     if elem == genome:
         self.collection.remove(genome)
 
-    def replace(self, genome):
-        """Excepcion en caso de que no esté el elemento a elminar
+    def replace(self, genomerm, genomerp):
+        """Replace genomerm with genomerp in the 'self' array
+        Excepcion en caso de que no esté el elemento a elminar
 
         Args:
-            genome:
+            genomerm(array): Genome class object
+            genomerp(array): Genome class object
         """
         # for elem in self.collection:
         #     if elem == genome:
-        self.remove(genome)
-        self.add(genome)
+        self.remove(genomerm)
+        self.add(genomerp)
 
 
 genome1 = Genome([1, 33, 4], 3)
@@ -65,3 +82,4 @@ print(prueba)
 
 prueba.remove(genome2)
 print(prueba)
+
