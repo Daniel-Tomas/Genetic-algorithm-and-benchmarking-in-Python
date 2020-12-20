d42d9424eefb7e61a7a6d77becbecb3da3af7cfb
AaronCaberoBlanco <71380398+AaronCaberoBlanco@users.noreply.github.com>
2020-12-20 09:15:57 +0100

    Subido lo nuevo

---
+++ group14/EA.py
@@ -0,0 +1,102 @@
+from group14.ImplementedClasses import *
+from group14.Population import Population
+import random
+from benchmarks import functions as fun
+
+
+class EA(object):
+    """This class is the entry point to the execution of the algorithm.
+
+    Attributes:
+        minfun (function): Function used to calculate the fitness of a genome.
+        bounds (list): Contains the minimum and maximum values that each variable can take from a candidate
+            solution.
+        population (Population): Object which contains a list of genomes.
+        best_genome (Genome): Optimal solution calculated by the algorithm.
+
+
+    Args:
+        minfun_ (function): The function to be set.
+        bounds_ (list): List of values to be set.
+        p_size (int): Maximum size of our population.
+    """
+
+    def __init__(self, minfun_, bounds_, p_size):
+        self.minfun = minfun_
+        self.bounds = bounds_
+        self.population = Population(minfun_, bounds_, p_size)
+        self.best_genome = None
+
+    def run(self, iterations):
+        """Runs the Differential Evolution algorithm.
+
+        Args:
+            iterations (int): Number of iterations to be made by the algorithm.
+        """
+        print(f'Before:\n {self.population}\n')
+        self.best()
+        print(f'Best Genome before: {self.best_genome.array}, fitness={self.best_genome.fitness} ')
+
+        mutator = Rand1MutationOperator(self.population, self.bounds, 0.2)
+        mixer = ExponentialCrossoverOperator(self.minfun)
+        replacer = ElitistReplacementOperator()
+
+        for _ in range(iterations):
+            candidate_population = Population(None, None, 0)
+            for target in self.population.collection:
+                # List with genomes who will be the donors
+                mutant = mutator.apply(target)
+                # Genome modified by replacing a few random positions
+                candidate_genome = mixer.apply(target, mutant)
+
+                candidate_population.add(candidate_genome)
+
+            # Targets are replaced by candidates from the population if candidate has less fitness than target
+            self.population = replacer.apply(self.population, candidate_population)
+
+        print(f'After:\n {self.population}\n')
+        self.best()
+        print(f'Best Genome after: {self.best_genome.array}, fitness={self.best_genome.fitness} ')
+
+    def best(self):
+        """Returns the best genome
+
+        Returns:
+            best_genome (Genome): Optimal solution calculated by the algorithm.
+        """
+        self.population.descendent_sort()
+        self.best_genome = self.population.collection[0]
+        return self.best_genome
+
+
+if __name__ == '__main__':
+    def f(array):
+        res = 0;
+        if (sum(array) > horas_estudio):
+            return 0;
+        for i in range(len(array)):
+            nota = (array[i] * punto_hora[i])
+            if (random.random() <= posibilidad_revision[i]):
+                nota += nota_revision[i];
+            if (nota > 10):
+                nota = 10
+            res += nota * creditos[i]
+        return res / sum(creditos);
+
+
+    notas_minimas = [3, 4, 5, 2, 1]
+    punto_hora = [1, 2, 1, 2, 3]
+    creditos = [3, 6, 1, 4, 2]
+    posibilidad_revision = [0.4, 0.3, 0.2, 0.5, 0.6]
+    nota_revision = [0.5, 0.1, 0.3, 0.4, 0.2]
+    horas_estudio = 31
+    asignaturas = len(creditos)
+    mybounds = []
+    for i in range(asignaturas):
+        mybounds.append((notas_minimas[i] / punto_hora[i], 10 / punto_hora[i]))
+
+    myEA = EA(f, mybounds, 30)
+
+    myEA.run(1000)
+
+    myEA.best()
---
+++ group14/ImplementedClasses.py
@@ -0,0 +1,162 @@
+from group14 import AbstractClasses
+import random
+import numpy as np
+
+from group14.Genome import Genome
+from group14.Population import Population
+
+
+class UniformSelectionOperator(AbstractClasses.SelectionOperator):
+    """Basic class
+
+    This class inherits from the abstract class 'SelectionOperator'.
+
+    Attributes:
+        population (Population): Object which contains a list of genomes.
+
+    Args:
+        population_ (Population): Population to be set.
+    """
+
+    def __init__(self, population_):
+        self.population = population_
+
+    def apply(self, target):
+        """Select three random genomes of current_population different from each other and from the target.
+
+        Args:
+            target (Genome): Genome object selected to work it.
+
+        Returns:
+            donors (array) : Contains three different genomes obtained randomly from the population.
+        """
+
+        donors = [target]
+
+        for _ in range(3):
+            genome = random.choice(self.population.collection)
+            while genome in donors:
+                genome = random.choice(self.population.collection)
+            donors.append(genome)
+
+        donors.remove(target)
+        return donors
+
+
+class Rand1MutationOperator(AbstractClasses.MutationOperator):
+    """Basic class
+
+    This class inherits from the abstract class 'MutationOperator'.
+
+    Attributes:
+        population (Population): Object which contains a list of genomes.
+        bounds (list): Contains the minimum and maximum values that each variable can take from a candidate
+                solution.
+        F (float): Used as a variable in the mutation operation.
+        selector (function): Function that selects three random genomes
+
+    Args:
+        population_ (Population): Population to be set.
+        bounds_ (list): List to be set.
+        F_ (float): Float to be set.
+    """
+
+    def __init__(self, population_, bounds_, F_=0.5):
+        self.population = population_
+        self.bounds = bounds_
+        self.F = F_
+        self.selector = UniformSelectionOperator(population_)
+
+    def apply(self, target):
+        """Generate a new genome by combining 'donors'.
+
+       Args:
+            target (Genome): Genome object selected to work it.
+
+        Returns:
+            Genome (Genome) : Returns the genome resulting from the combination of the three genomes passed as a
+            parameter in the 'donors' array.
+        """
+        donors = self.selector.apply(target)
+        mutant_array = []
+        for i in range(len(self.bounds)):
+            min_ = self.bounds[i][0]
+            max_ = self.bounds[i][1]
+            subtract_res = donors[1].array[i] - donors[2].array[i]
+            multiplication_res = self.F * subtract_res
+            addition_res = donors[0].array[i] + multiplication_res
+            if addition_res < min_:
+                addition_res = min_
+            elif addition_res > max_:
+                addition_res = max_
+            mutant_array = np.append(mutant_array, addition_res)
+        return Genome(array=mutant_array, fitness=0)
+
+
+class ExponentialCrossoverOperator(AbstractClasses.CrossoverOperator):
+    """Basic class
+
+    This class inherits from the abstract class 'CrossoverOperator'.
+
+    Attributes:
+        minfun (function): Function used to calculate the fitness of a genome.
+        CR (float): This parameter will be used as a condition to continue doing the crossover operation.
+
+    Args:
+        minfun (function): Function to be set.
+        CR (float): float to be set.
+    """
+
+    def __init__(self, minfun, CR=0.1):
+        self.minfun = minfun
+        self.CR = CR
+
+    def apply(self, target, mutant):
+        """Returns a new candidate by mixing 'target' and 'mutant'.
+
+        Args:
+            target (Genome): Genome object selected to work it.
+            mutant (Genome): The genome which we are going to combine with 'target'.
+
+        Returns:
+            Genome (Genome) : Is the genome resulting from the combination between 'target' and 'mutant'.
+        """
+        size = len(target.array)
+        j = random.randint(0, size - 1)
+        candidate = Genome(array=target.array)
+
+        candidate.array[j] = mutant.array[j]
+        j = (j + 1) % size
+        i = 1
+        while i < size and random.random() < self.CR:
+            candidate.array[j] = mutant.array[j]
+            j = (j + 1) % size
+            i = i + 1
+
+        return Genome(array=candidate.array, fitness=self.minfun(candidate.array))
+
+
+class ElitistReplacementOperator(AbstractClasses.ReplacementOperator):
+    """Basic class
+
+    This class inherits from the abstract class 'ReplacementOperator'.
+    """
+
+    def apply(self, current_population, candidate_population):
+        """
+        Each genome of 'current_population' is compared with the genome which is in the same position than
+        'candidate_population' and the one with the best fitness of these two genomes is added to
+        'result_population'.
+
+        Args:
+            current_population (Population): Object which contains a list of genomes.
+            candidate_population (list): List which contains a list of genomes.
+
+        Returns:
+            result_population (Population) : Resulting population with the best fitness in each position.
+        """
+        result_population = Population(None, None, 0)
+        for target, candidate in zip(current_population.collection, candidate_population.collection):
+            result_population.add(candidate if candidate.fitness > target.fitness else target)
+
+        return result_population
