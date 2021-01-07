6a1693c596601b8faf9bffdbf529606eca8efddb
pabelsbf <pabelspbf@gmail.com>
2021-01-07 16:27:24 +0100

    Actualizar

--- src/Data.py
+++ src/Data.py
@@ -4,4 +4,4 @@ credits = [3, 6, 1, 4, 2]
 revision_probability = [0.4, 0.3, 0.2, 0.5, 0.3]
 student_luck = [0.3, 0.2, 0.4, 0.5, 0.4]
 revision_mark = [0.5, 0.1, 0.3, 0.4, 0.2]
-study_hours = 20
+study_hours = 16
